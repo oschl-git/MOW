3dd93e69fb3593f8d5729e1426e7a144e2bacd9e
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-03-01 14:43:11 +0100

    Displaying questions now functional

--- public/scripts/practice.js
+++ public/scripts/practice.js
@@ -1,39 +1,64 @@
 // References to HTML elements:
-let practice1Button = document.querySelector('#practice-button-1');
-let practice2Button = document.querySelector('#practice-button-2');
-let practice3Button = document.querySelector('#practice-button-3');
-let practice4Button = document.querySelector('#practice-button-4');
+const practice1Button = document.querySelector('#practice-button-1');
+const practice2Button = document.querySelector('#practice-button-2');
+const practice3Button = document.querySelector('#practice-button-3');
+const practice4Button = document.querySelector('#practice-button-4');
 
-let beginContainer = document.querySelector('#begin-container');
-let practiceSelectContainer = document.querySelector('#practice-select-container');
-let practiceWriteContainer = document.querySelector('#practice-write-container');
-let endContainer = document.querySelector('#end-container');
+const beginContainer = document.querySelector('#begin-container');
+const practiceSelectContainer = document.querySelector('#practice-select-container');
+const practiceWriteContainer = document.querySelector('#practice-write-container');
+const endContainer = document.querySelector('#end-container');
+
+const answerButton1 = document.querySelector('#answer-button-1');
+const answerButton2 = document.querySelector('#answer-button-2');
+const answerButton3 = document.querySelector('#answer-button-3');
+
+const selectQuestion = document.querySelector('#select-question');
 
 
 // Event listeners:
+// Begin buttons:
 practice1Button.addEventListener('click', (e) => {
 	switchToView('practice-select');
-	reversedQuestions = false;
+	reverseQuestions = false;
+	selectSetup();
 });
 
 practice2Button.addEventListener('click', (e) => {
 	switchToView('practice-select');
-	reversedQuestions = true;
+	reverseQuestions = true;
+	selectSetup();
 });
 
 practice3Button.addEventListener('click', (e) => {
 	switchToView('practice-write');
-	reversedQuestions = false;
+	reverseQuestions = false;
 });
 
 practice4Button.addEventListener('click', (e) => {
 	switchToView('practice-write');
-	reversedQuestions = true;
+	reverseQuestions = true;
+});
+
+// Practice answer buttons:
+answerButton1.addEventListener('click', (e) => {
+	handleAnswerButton(1);
+});
+
+answerButton2.addEventListener('click', (e) => {
+	handleAnswerButton(2);
+});
+
+answerButton3.addEventListener('click', (e) => {
+	handleAnswerButton(3);
 });
 
 
 // General variables:
-let reversedQuestions = false;
+let reverseQuestions = false;
+let score = 0;
+let shuffledQuestions = shuffleArray(questions);
+let questionIndex = 0;
 
 
 // Code to run on load:
@@ -52,4 +77,43 @@ function switchToView(newView) {
 		item.style.display = 'none';
 	}
 	views.get(newView).style.display = 'flex';
+}
+
+
+function shuffleArray(array) {
+	let newArray = [...array];
+	for (var i = newArray.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = newArray[i];
+		newArray[i] = newArray[j];
+		newArray[j] = temp;
+	}
+	return newArray;
+}
+
+// Select practice:
+function selectSetup() {
+	score = 0;
+	questionIndex = 0;
+	displaySelectQuestion(shuffledQuestions[questionIndex]);
+	questionIndex++;
+}
+
+function handleAnswerButton(button) {
+	displaySelectQuestion(shuffledQuestions[questionIndex]);
+	questionIndex++;
+}
+
+function displaySelectQuestion(question) {
+	selectQuestion.textContent = question.question;
+	let answerButtons = shuffleArray([answerButton1, answerButton2, answerButton3]);
+	answerButtons[0].textContent = question.answer;
+	answerButtons.shift();
+	for (const button of answerButtons) {
+		let answer;
+		do {
+			answer = shuffledQuestions[Math.floor(Math.random() * shuffledQuestions.length)].answer;
+		} while (answer == question.answer);
+		button.textContent = answer;
+	}
 }
